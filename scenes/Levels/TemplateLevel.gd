3afaa161dba2b4fa17d58d1073523968e1ce35dc
Gaspard Culis <gaspard.culis@gmail.com>
2023-01-23 17:38:59 +0100

    Fix worldSpawn and SpriteSheet collisions

--- scenes/Levels/TemplateLevel.gd
+++ scenes/Levels/TemplateLevel.gd
@@ -2,10 +2,12 @@ extends TileMap
 
 
 const PlayerScene = preload("res://scenes/Player/Player.tscn")
+onready var WorldSpawn = $WorldSpawn
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var instance = PlayerScene.instance()
 	self.add_child(instance)
+	instance.global_position = WorldSpawn.global_position
 
