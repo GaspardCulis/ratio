96961dbe6710ee7c5285aa1cb74ee7922206ecbb
Gaspard Culis <gaspard.culis@gmail.com>
2023-01-23 18:22:37 +0100

    Made Abstract leevel class

--- scenes/Levels/TemplateLevel.gd
+++ scenes/Levels/TemplateLevel.gd
@@ -1,13 +1,1 @@
-extends TileMap
-
-
-const PlayerScene = preload("res://scenes/Player/Player.tscn")
-onready var WorldSpawn = $WorldSpawn
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var instance = PlayerScene.instance()
-	self.add_child(instance)
-	instance.global_position = WorldSpawn.global_position
-
+extends "res://src/abstract/AbstractLevel.gd"
