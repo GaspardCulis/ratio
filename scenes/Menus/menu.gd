2616d0097aee0df4fe79d1a3dff16902d45e7fa6
Jaauf <97916233+Jaauf@users.noreply.github.com>
2023-01-24 15:47:21 +0100

    reparation de niveau de la pluie

--- scenes/Menus/menu.gd
+++ scenes/Menus/menu.gd
@@ -1,11 +1,5 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/Start.grab_focus()
