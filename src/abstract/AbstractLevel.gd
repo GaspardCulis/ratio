df8ce57c5aa21af9a44749c2cdc1c4f6daf2e4cd
Gaspard Culis <gaspard.culis@gmail.com>
2023-01-24 14:00:31 +0100

    Swoiotched scene in abstract boy

--- src/abstract/AbstractLevel.gd
+++ src/abstract/AbstractLevel.gd
@@ -2,6 +2,7 @@ class_name Level
 extends TileMap
 
 export(NodePath) var WorldSpawn
+export(Resource) var otherDim: Resource
 
 const PlayerScene = preload("res://scenes/Player/Player.tscn")
 onready var WorldSpawnNode = get_node(WorldSpawn)
@@ -12,3 +13,9 @@ func _ready():
 	var instance = PlayerScene.instance()
 	self.add_child(instance)
 	instance.global_position = WorldSpawnNode.global_position
+	
+func _input(event):
+	if Input.is_action_just_pressed("switch_dim"):
+		if (otherDim):
+			get_tree().change_scene(otherDim.resource_path)
+		
