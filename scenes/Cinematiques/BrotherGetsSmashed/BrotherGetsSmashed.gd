69e162d152a124a7a24731a38392fd4725f75c72
Gaspard Culis <gaspard.culis@gmail.com>
2023-01-24 21:13:36 +0100

    Intro

---
+++ scenes/Cinematiques/BrotherGetsSmashed/BrotherGetsSmashed.gd
@@ -0,0 +1,31 @@
+extends TileMap
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("jemedeplace")
+	$AnimationPlayer.playback_speed = 1
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("RESET")
+	$AnimationPlayer.play("dialogue")
+	yield($AnimationPlayer, "animation_finished")
+	yield(display_text("Que faites vous la professeur Blanchon ?\nJe vois que l'experience de mutation a marche ! Vous avez une sale tete... Vous m'entendez ?"), "completed")
+	$nani.play()
+	$Label.text = ""
+	$AnimationPlayer.play("nani")
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("exit")
+	yield($AnimationPlayer, "animation_finished")
+	get_tree().change_scene("res://scenes/Levels/WatchFragments/WatchFragmentsScene.tscn")
+
+func display_text(text: String):
+	for i in range(text.length()):
+		$Label.text = text.substr(0, i+1)
+		$Boup.pitch_scale = 1 + (randf() - 0.5)*0.2
+		$Boup.play()
+		if text[i] in ['!', '?', ';', ',', '.']:
+			yield(get_tree().create_timer(0.5), "timeout")
+		else:
+			yield(get_tree().create_timer(0.1), "timeout")
