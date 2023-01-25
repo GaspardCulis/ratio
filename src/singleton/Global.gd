57388f3a742901093c4afa4c400dce17ca3bd0d3
Gaspard Culis <gaspard.culis@gmail.com>
2023-01-25 01:15:11 +0100

    Gros changement

--- src/singleton/Global.gd
+++ src/singleton/Global.gd
@@ -1,35 +1,11 @@
 extends Node
 
-var has_saved := false
-var player_pos_save := Vector2.ZERO
 var onTitle = true
 var showFPS = false
 var currentDimension = "Present"
 
-var player_velocity_save := Vector2.ZERO
-var player_animation_state := ""
-var player_animation_frame := 0
-var player_flip := 1
-
 #Music
 var isOnMusic = false
-
-func load_saved_player_state(player: KinematicBody2D):
-	if has_saved:
-		player.global_position = player_pos_save
-		player.velocity = player_velocity_save
-		player.AnimationSprite.animation = player_animation_state
-		player.AnimationSprite.frame = player_animation_frame
-		player.AnimationSprite.scale.x = player_flip
-		player.get_camera().reset_smoothing()
-		
-func save_player_state(player: KinematicBody2D):
-	player_pos_save = player.global_position
-	player_velocity_save = player.velocity
-	player_animation_state = player.AnimationSprite.animation
-	player_animation_frame = player.AnimationSprite.frame
-	player_flip = player.AnimationSprite.scale.x
-	has_saved = true
 	
 const transTexture = preload("res://assets/texture/blan.png")
 func change_scene(path: String):
