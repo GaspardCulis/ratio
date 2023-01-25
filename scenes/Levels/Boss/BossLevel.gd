a103a6cf6a39ae92a51c82f0ad2d5d9b7d1f880b
Milan Soragna <milan.soragna@gmail.com>
2023-01-25 07:13:31 +0100

    Revert to good changes

--- scenes/Levels/Boss/BossLevel.gd
+++ scenes/Levels/Boss/BossLevel.gd
@@ -1,4 +1,13 @@
 extends "res://src/abstract/AbstractLevel.gd"
 
+export var portePath: NodePath
+onready var porte = get_node(portePath)
+
 func _ready():
-	yield(get_tree().create_timer(10),"timeout")
+	#yield(get_tree().create_timer(10),"timeout")
+	$HoundBoss.connect("boss_dead", self, "_on_TileMap_boss_dead")
+	pass
+
+func _on_TileMap_boss_dead():
+	print("Test")
+	porte.frame = 1
